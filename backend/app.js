cb69c0c75fa56b34261c3cf8981aef288ade4b44
ananicole-sr <ana.nicolesanchezr@gmail.com>
2026-03-07 11:20:19 -0600

    Start App

---
+++ backend/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const PORT = 3000;
+
+app.use(cors());
+
+
+app.listen(PORT, () => {
+    console.log(`Listening in Port: ${PORT}`)
+})
+
