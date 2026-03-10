fd2895204fcdb518efe01e2790bddfc8dddd630c
ananicole-sr <ana.nicolesanchezr@gmail.com>
2026-03-10 15:52:27 -0600

    Added Catalog Page and Search Form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,15 +3,15 @@ import './App.css'
 import Tarjeta from './components/Tarjeta.jsx'
 import ListaTarjeta from "./components/ListaTarjeta.jsx";
 import Detalles from "./pages/Detalles.jsx";
+import Catalogo from "./pages/Catalogo.jsx";
 
 function App() {
   return (
     <>
     <BrowserRouter> 
       <Routes>
-       <Route path="/escuelas" element={<ListaTarjeta/>} />
+       <Route path="/escuelas" element={<Catalogo/>} />
        <Route path="/escuelas/:id" element={<Detalles/>} />
-
       </Routes>
     </BrowserRouter>
     </>)
