c04a682180bbd6220a722a84d96d5d9cf858d68d
PEVA14 <pablovargas.astorga@gmail.com>
2026-03-12 10:42:27 -0600

    gave styling to navbar and footer, working on another component

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,16 +1,19 @@
 import { useNavigate } from 'react-router-dom';
 import logo from "../assets/logoMEP.png";
+import "../components/NavBar.css"
 
 export default function NavBar(){
     const navigate = useNavigate();
 
     return(
-        <div>
-            <img src = {logo} alt="Mi Escuela Primero Logo.png"/>
-            <button onClick={()=>navigate('/')}>Inicio</button>
-            <button onClick={()=>navigate('/escuelas')}>Explorar Escuelas</button>
-            <button onClick={()=>navigate('/#explainer')}>Como funciona</button>
-            {/* <button onClick={()=>navigate('')}>Contacto</button> */}
-        </div>
+        <nav className="navbar">
+            <img className="navbar__logo" src={logo} alt="Mi Escuela Primero Logo.png" />
+            <div className="navbar__links">
+                <button className="navbar__button" onClick={() => navigate('/')}>Inicio</button>
+                <button className="navbar__button" onClick={() => navigate('/escuelas')}>Explorar Escuelas</button>
+                <button className="navbar__button" onClick={() => navigate('/#explainer')}>Como funciona</button>
+                {/* <button className="navbar__button" onClick={() => navigate('')}>Contacto</button> */}
+            </div>
+        </nav>
     )
 }
